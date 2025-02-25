38b959a117665838312a6d0f44df77428e67b142
Zarazenek <153453383+ZARAZANEK@users.noreply.github.com>
2025-02-25 22:31:56 +0200

    Update seconds.py

--- Project_1/seconds.py
+++ Project_1/seconds.py
@@ -1,19 +1,1 @@
 
-import phonenumbers
-from phonenumbers import geocoder, carrier
-
-# Парсинг номера телефону
-x = phonenumbers.parse('+380 97 207 9224')
-
-# Отримання назви оператора
-Carrier = carrier.name_for_number(x, 'en')
-
-# Отримання опису регіону
-Region = geocoder.description_for_number(x, 'en')
-
-# Спроба отримати назву міста
-citi = geocoder.description_for_number(x, "en")
-
-print("Carrier:", Carrier)
-print("Region:", Region)
-print("City:", citi if citi else "Місто не визначено")
